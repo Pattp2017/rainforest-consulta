8a4bbd9cfb58694693be602773b4df641cc3df6f
sstrgcerte-cloud <sstrgcerte@gmail.com>
2026-08-06 15:56:09 -0300

    Update app.js

--- jss/app.js
+++ jss/app.js
@@ -1,1 +1,12 @@
 
+document.addEventListener('DOMContentLoaded', iniciarSistema);
+
+async function iniciarSistema() {
+  try {
+    const resultado = await testarConexaoSupabase();
+
+    console.log('Supabase conectado com sucesso:', resultado);
+  } catch (erro) {
+    console.error('Erro ao conectar ao Supabase:', erro);
+  }
+}
